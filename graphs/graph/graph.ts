23d27b1c258cf75193b3ffc5b9116a6e0df3f9af
Nicholas <nick.orsi1992@gmail.com>
2024-03-01 12:28:07 -0800

    Solved graph problem.

--- graphs/graph/graph.ts
+++ graphs/graph/graph.ts
@@ -37,6 +37,13 @@ class GraphStr {
     v2.adjacent.add(v1);
   }
 
+  /** Add edges between pairs of nodes passed into the array. */
+  addEdges(nodePairsArray: [v1: GNodeStr, v2: GNodeStr][]): void {
+    for(const nodePair of nodePairsArray) {
+      this.addEdge(nodePair[0], nodePair[1])
+    }
+  }
+
   /** Remove edge between v1 and v2. */
   removeEdge(v1: GNodeStr, v2: GNodeStr): void {
     v1.adjacent.delete(v2);
@@ -45,6 +52,9 @@ class GraphStr {
 
   /** Remove node from graph. */
   removeNode(node: GNodeStr): void {
+    for(const nodePair of node.adjacent) {
+      this.removeEdge(node, nodePair);
+    }
     this.nodes.delete(node);
   }
 }
