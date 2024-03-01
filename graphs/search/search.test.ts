980bbb68d5da34ba610b27ad5f08a7a2223ddd8c
Nicholas <nick.orsi1992@gmail.com>
2024-03-01 12:29:19 -0800

    Added joel's test fix
    Removes a solution that was previously not possible
    Makes it so each appraoch (iteractive and recursive tests) produce the same result.

--- graphs/search/search.test.ts
+++ graphs/search/search.test.ts
@@ -2,27 +2,40 @@ import { describe, expect, it } from "vitest";
 import { GraphStr } from "../common/graph";
 import { bfs, iDfs, rDfs } from "./search";
 
-describe("DFS", function () {
-  it("returns array of nodes using rDfs", function () {
-    //    d -- b -- a -- c
-    //     \------------/
+// describe("DFS", function () {
+//   it("returns array of nodes using rDfs", function () {
+//     //    d -- b -- a -- c
+//     //     \------------/
 
+//     const graph = new GraphStr();
+//     const [a, b, c, d] = graph.addFromVals(["a", "b", "c", "d"]);
+//     graph.addEdges([[d, b], [b, a], [a, c], [c, d]]);
+//     const rez = rDfs(a).join("-");
+//     expect([ "a-c-b-d", "a-b-d-c", "a-c-d-b" ]).toContain(rez);
+//   });
+
+//   it("returns array of nodes using iDfs", function () {
+//     //    d -- b -- a -- c
+//     //     \------------/
+
+//     const graph = new GraphStr();
+//     const [a, b, c, d] = graph.addFromVals(["a", "b", "c", "d"]);
+//     graph.addEdges([[d, b], [b, a], [a, c], [c, d]]);
+//     const rez = iDfs(a).join("-");
+//     expect([ "a-c-b-d", "a-b-d-c", "a-c-d-b" ]).toContain(rez);
+//   });
+// });
+
+describe("DFS", function () {
+  it.each([
+    ["iDFS", iDfs],
+    ["rDFS", rDfs],
+  ])("DFS does a-b-d-c or a-c-d-b (%s)", function (name, fn) {
     const graph = new GraphStr();
     const [a, b, c, d] = graph.addFromVals(["a", "b", "c", "d"]);
     graph.addEdges([[d, b], [b, a], [a, c], [c, d]]);
     const rez = rDfs(a).join("-");
-    expect([ "a-c-b-d", "a-b-d-c", "a-c-d-b" ]).toContain(rez);
-  });
-
-  it("returns array of nodes using iDfs", function () {
-    //    d -- b -- a -- c
-    //     \------------/
-
-    const graph = new GraphStr();
-    const [a, b, c, d] = graph.addFromVals(["a", "b", "c", "d"]);
-    graph.addEdges([[d, b], [b, a], [a, c], [c, d]]);
-    const rez = iDfs(a).join("-");
-    expect([ "a-c-b-d", "a-b-d-c", "a-c-d-b" ]).toContain(rez);
+    expect(["a-b-d-c", "a-c-d-b"]).toContain(rez);
   });
 });
 
